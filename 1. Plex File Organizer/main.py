ab0a1687283cca4dd2cf3cc7a4dc14967d0b8531
14thDovePin <therlaf0409@gmail.com>
2025-06-30 17:24:14 +0800

    Created A New Script #1
    - Created "Plex File Organizer" & main.
    - Updated `README.md`.
    - Created a template for future main files.

---
+++ 1. Plex File Organizer/main.py
@@ -0,0 +1,22 @@
+""" June 30, 2025
+What is this script about?
+This script formats any Movie or Series file names into their proper
+titles that are readable by Plex's media file namescheming.
+
+How does it work?
+Below is the script's procedure. (may change as the script is developed)
+
+1. Locate the files for the media.
+2. Determine media type.
+3. Obtain the title from the movies metadata from OMDb or IMDb.
+4. Format title to Plex's media file namescheming.
+5. Rename all media files.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_template.py
@@ -0,0 +1,17 @@
+""" June 30, 2025
+What is this script about?
+Describe the the script does.
+
+How does it work?
+Below is the script's procedure. (may change as the script is developed)
+
+1. List down each process.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
