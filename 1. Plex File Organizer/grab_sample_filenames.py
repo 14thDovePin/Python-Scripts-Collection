5ecd33cd047407692b2105e706f3c962b8a353b8
14thDovePin <therlaf0409@gmail.com>
2025-07-03 23:48:56 +0800

    #1 Added Selenium Base for Sampling
    - Added gitignore

--- 1. Plex File Organizer/grab_sample_filenames.py
+++ 1. Plex File Organizer/grab_sample_filenames.py
@@ -1,5 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import website_ignore as website
+
+
 def grab_sample_filenames():
     """Return 50 samples each of movie & tv show file names."""
+    driver = webdriver.Chrome()
+    driver.get(website.root())
+    input("Press Any Key to Close")
+    driver.close()
 
 
 if __name__ == "__main__":
