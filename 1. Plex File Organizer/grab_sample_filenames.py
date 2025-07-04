1d01b01a53b0b0827f3ea92363084c3f8ed6d1b4
14thDovePin <therlaf0409@gmail.com>
2025-07-04 20:19:04 +0800

    #1 Reorganized Code
    - Reorganized code to be reusable and a little more modular.
    - Added a testing mode flag.
    - Added script dev note.

--- 1. Plex File Organizer/grab_sample_filenames.py
+++ 1. Plex File Organizer/grab_sample_filenames.py
@@ -1,3 +1,12 @@
+""" July 4, 2025
+DEV NOTE
+This script is a special case script. It's not supposed to be
+reused on other systems or for any other. This script can and will
+break in the near future due to the nature of it relying on
+selenium and the front end of a website entirely.
+"""
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -5,27 +14,22 @@ from selenium.webdriver.common.by import By
 import website_ignore as website
 
 
+TEST_MODE = True
+
+
 def grab_sample_filenames():
     """Return 50 samples each of movie & tv show file names."""
     driver = webdriver.Chrome()
 
     # Get Page Links
-    driver.get(website.movies())
+    page_links = get_page_links(driver, website.movies())
 
-    page_list_elements = driver.find_elements(
-        By.XPATH,
-        "//div[@class='pagination']//ul//li//a"
-    )
-
-    page_links = [i.get_attribute('href') for i in page_list_elements][:-2]
-
-    # Loop Through Web Pages
+    # Loop Through Web Pages & Get Links
     movie_links = []
-
     for page in page_links:
         driver.get(page)
 
-        # Get Movies Links
+        # Grab Movie Links
         movies_elements = driver.find_elements(
             By.XPATH,
             # "//th[@class='coll-1 name']"
@@ -36,14 +40,39 @@ def grab_sample_filenames():
             href = movie_element.get_attribute('href')
             movie_links.append(href)
 
-    # movies_hrefs = [i.get_attribute('href') for i in movies_elements]
+    # Loop through each Movie Links & get the Sample File Names.
+    for movie_link in movie_links:
+        driver.get(movie_link)
 
-    [print(i) for i in movie_links]
+        file_tab_element = driver.find_element(
+            By.XPATH,
+            "//div[@class='tab-nav clearfix']//ul//li[2]"
+        )
 
+        # Extract File Name
+        file_tab_element.click()
 
 
     input("\nPress Any Key to Close\n\n")
     driver.close()
+
+
+def get_page_links(driver: webdriver, link: str) -> list:
+    """Return the pages web links on a given link."""
+    driver.get(link)
+
+    page_list_elements = driver.find_elements(
+        By.XPATH,
+        "//div[@class='pagination']//ul//li//a"
+    )
+
+    page_links = [i.get_attribute('href') for i in page_list_elements][:-2]
+
+    if TEST_MODE:
+        page_links = page_links[:1]
+
+    return page_links
+
 
 
 if __name__ == "__main__":
