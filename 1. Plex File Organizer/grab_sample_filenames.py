32323cefb823a524957d2b8f455f92afab1f8cbd
14thDovePin <therlaf0409@gmail.com>
2025-07-04 19:50:07 +0800

    #1 Implemented Movie Samples Get Link

--- 1. Plex File Organizer/grab_sample_filenames.py
+++ 1. Plex File Organizer/grab_sample_filenames.py
@@ -8,8 +8,41 @@ import website_ignore as website
 def grab_sample_filenames():
     """Return 50 samples each of movie & tv show file names."""
     driver = webdriver.Chrome()
-    driver.get(website.root())
-    input("Press Any Key to Close")
+
+    # Get Page Links
+    driver.get(website.movies())
+
+    page_list_elements = driver.find_elements(
+        By.XPATH,
+        "//div[@class='pagination']//ul//li//a"
+    )
+
+    page_links = [i.get_attribute('href') for i in page_list_elements][:-2]
+
+    # Loop Through Web Pages
+    movie_links = []
+
+    for page in page_links:
+        driver.get(page)
+
+        # Get Movies Links
+        movies_elements = driver.find_elements(
+            By.XPATH,
+            # "//th[@class='coll-1 name']"
+            "//div[@class='featured-list']//div//table//tbody//tr//td//a[2]"
+        )
+
+        for movie_element in movies_elements:
+            href = movie_element.get_attribute('href')
+            movie_links.append(href)
+
+    # movies_hrefs = [i.get_attribute('href') for i in movies_elements]
+
+    [print(i) for i in movie_links]
+
+
+
+    input("\nPress Any Key to Close\n\n")
     driver.close()
 
 
