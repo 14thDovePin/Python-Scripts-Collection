13495a43622892889ab44b6015c58569dce78450
14thDovePin <therlaf0409@gmail.com>
2025-07-10 18:15:11 +0800

    #1 Deprecated Title Checking Implementation

--- 1. Plex File Organizer/request_omdb.py
+++ 1. Plex File Organizer/request_omdb.py
@@ -1,3 +1,4 @@
+import math
 import requests
 import os
 
@@ -62,21 +63,22 @@ def request_data(processed_filename: dict):
 
     # Attempt to match metadata with processed filename.
     url = construct_request(title_sequence, year)
-    print(title_sequence)
-    print(url)
     metadata = requests.get(url).json()
 
-    # Failsafe, attempt to match for each consecutive sequenced words.
-    if metadata['Response'] == 'False':
-        temp_ts = []
+    # # Failsafe, attempt to match for each consecutive sequenced words.
+    # if metadata['Response'] == 'False':
+    #     temp_ts = []
 
-        for word in title_sequence:
-            temp_ts.append(word)
-            url = construct_request(temp_ts, year)
-            metadata = requests.get(url).json()
+    #     for word in title_sequence:
+    #         temp_ts.append(word)
+    #         url = construct_request(temp_ts, year)
+    #         metadata = requests.get(url).json()
 
-            if metadata['Response'] == 'True':
-                break
+    #         if metadata['Response'] == 'True':
+    #             # Title Check
+    #             title = metadata['Title']
+    #             if check_title(title, title_sequence):
+    #                 break
 
     if metadata['Response'] == 'False':
         print("Processing Failed! Match not found...")
@@ -87,12 +89,30 @@ def request_data(processed_filename: dict):
     return metadata
 
 
+def check_title(title: str, title_sequence: list) -> bool:
+    """Check if title contains at least 2/3 of the title_squence."""
+    # Calculate word count based on criteria.
+    count = math.ceil(len(title_sequence)*2/3)
+    check = 0
+
+    # Loop through each word.
+    for word in title_sequence[:count]:
+        if word in title:
+            check += 1
+
+    # Check if word count satisfies criteria.
+    if check >= count:
+        return True
+
+    return False
+
+
 # Sample
 pf = {
     "title" : '',
-    "title_sequence": ['scary', 'movie'],
+    "title_sequence": ['Aile', 'Saadeti', 'ABC'],
     "type" : '',
-    "year" : '2013',
+    "year" : '',
     "season" : 0,
     "episode" : 0,
     "file_extension" : '',
@@ -100,6 +120,3 @@ pf = {
 }
 
 tr = request_data(pf)
-
-
-
