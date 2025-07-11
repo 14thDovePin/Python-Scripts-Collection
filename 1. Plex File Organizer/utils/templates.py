62d1150835a9acdca27de5c68e5d76585b4b61ea
14thDovePin <therlaf0409@gmail.com>
2025-07-11 22:44:17 +0800

    #1 Organized Code, Added Utils

---
+++ 1. Plex File Organizer/utils/templates.py
@@ -0,0 +1,30 @@
+def generate_metadata() -> dict:
+    """Return a template dict of a title's metadata.
+
+    Returns
+    -------
+    metadata : dict
+        Dictionary Keys:
+        - **title** : str
+        - **title_sequence** : list
+          An ordered list of string composed from the title.
+        - **type** : str
+          Can be either "movie", "season", or "episode".
+        - **year** : str
+        - **season** : str
+        - **episode** : str
+        - **file_extension** : str
+        - **imdb_id** : str
+    """
+    metadata = {
+        "title" : '',
+        "title_sequence": [],
+        "type" : '',
+        "year" : '',
+        "season" : '',
+        "episode" : '',
+        "file_extension" : '',
+        "imdb_id" : ''
+    }
+
+    return metadata
