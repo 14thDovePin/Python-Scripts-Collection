ea5e01535d5b1b0c3bdbebf95686777bf181d7c3
14thDovePin <therlaf0409@gmail.com>
2025-07-18 07:36:29 +0800

    #2 New Script For Tracknig Program Runtime

---
+++ 2. Track Software Runtime/main.py
@@ -0,0 +1,41 @@
+""" July 18, 2025
+What is this script about?
+This script acts like a watchdog for specific softwares. When it detects a software
+that is part of the watchlist, it will log that time and date into a data file, likewise
+it will also log when it can't detect the software/program anymore.
+
+This script is also designed to be paired with NSSM to run as a Windows service.
+
+How does it work?
+Below is the script's procedure. (may change as the script is developed)
+
+1. Scan windows processes.
+2. Check if any any of the listed programs matches a case inside the processes.
+3. Log matched cases and flag progam in list as running.
+4. If a program flagged as running has not been detected, log it in the file.
+
+External Libraries Used:
+  psutil
+
+Developer Notes
+I'm going to make this script for a specific case for now, in the future
+I might add extra features that allows integration and tracking of more than
+a single program.
+"""
+from time import sleep
+
+import psutil
+
+
+def main():
+    """Main function of the script"""
+
+
+    # Rest
+    sleep(5)
+
+
+if __name__ == "__main__":
+    # Run Script
+    while True:
+        main()
