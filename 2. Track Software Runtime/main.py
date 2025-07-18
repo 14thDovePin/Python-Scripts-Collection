9a08695ed924de6eeee747a5ffef7794e1fc090d
14thDovePin <therlaf0409@gmail.com>
2025-07-18 09:47:30 +0800

    #2 Implemented Data Structure & Time Processing

--- 2. Track Software Runtime/main.py
+++ 2. Track Software Runtime/main.py
@@ -23,7 +23,7 @@ I might add extra features that allows integration and tracking of more than
 a single program.
 """
 import json
-from time import sleep
+import time
 
 import psutil
 
@@ -38,7 +38,7 @@ def main():
         print("Fail")
 
     # Rest
-    sleep(5)
+    time.sleep(5)
 
 
 def find_process(process_name: str) -> psutil.Process:
@@ -58,6 +58,67 @@ def find_process(process_name: str) -> psutil.Process:
             pass
 
     return None
+
+
+def build_data(process_name: str, data: dict) -> None:
+    """Builds the data structure of a process inside a given dictionary."""
+    data[process_name.lower()] = {
+        "raw_seconds" : int(),
+        "raw_minutes" : int(),
+        "raw_hours" : int(),
+        "raw_days" : int(),
+        "raw_months" : int(),
+        "raw_years" : int(),
+        "seconds" : int(),
+        "minutes" : int(),
+        "hours" : int(),
+        "days" : int(),
+        "months" : int(),
+        "years" : int(),
+        "time_elapsed" : int()
+    }
+
+
+def process_time(seconds: int, data: dict) -> None:
+    """Converts raw seconds to their respective format to insert in a give dict data."""
+    gm_time = time.gmtime(seconds)
+
+    # Calculate Raw Time
+    raw_mins = seconds // 60
+    raw_hours = raw_mins // 60
+    raw_days = raw_hours // 24
+    raw_months = raw_mins // 43830
+    raw_years = raw_mins // 525960
+
+    data["raw_seconds"] = seconds
+    data["raw_minutes"] = raw_mins
+    data["raw_hours"] = raw_hours
+    data["raw_days"] = raw_days
+    data["raw_months"] = raw_months
+    data["raw_years"] = raw_years
+
+    # Calculate Time
+    years = seconds // 31557600
+    seconds -= years * 31557600 if years > 0 else 0
+
+    months = seconds // 2629800
+    seconds -= months * 2629800 if months > 0 else 0
+
+    days = seconds // 86400
+    seconds -= days * 86400 if days > 0 else 0
+
+    hours = seconds // 3600
+    seconds -= hours * 3600 if hours > 0 else 0
+
+    minutes = seconds // 60
+    seconds -= minutes * 60 if minutes > 0 else 0
+
+    data["seconds"] = seconds
+    data["minutes"] = minutes
+    data["hours"] = hours
+    data["days"] = days
+    data["months"] = months
+    data["years"] = years
 
 
 if __name__ == "__main__":
